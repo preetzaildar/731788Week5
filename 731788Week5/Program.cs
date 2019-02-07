9eb25665e7909eac92dd882ea2b8b221758d7f99
visualstudio <visualstudio@230-PC13>
2019-02-07 14:30:34 -0500

    2:30

---
+++ 731788Week5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _731788Week5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
